b19dcf733bbaaed2d6d28722276858c2c3fee86a
Mohammed Hasan Gani <mohammed.gani@dunelm.com>
2020-11-10 17:16:12 +0000

    Added customer and orders models

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+select
+    id as payment_id,
+    orderid as order_id,
+    amount/100 as amount,
+    status
+
+from raw.stripe.payment
+
+where status = 'success'
